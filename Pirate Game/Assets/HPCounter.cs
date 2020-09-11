9ffa332aba62ae310b739a56a62915cb783e4648
David Mees Knijnik <kdavidk98@gmail.com>
2020-09-10 21:23:27 -0300

    Health display

---
+++ Pirate Game/Assets/HPCounter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HPCounter : MonoBehaviour
+{
+    public TextMesh text;
+    public Ship ship;
+    Quaternion originalRotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalRotation = transform.rotation;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = ship.health.ToString();
+        transform.rotation = originalRotation;
+    }
+}
---
+++ Pirate Game/Assets/Scripts/PlayCamera.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayCamera : MonoBehaviour
+{
+    public Camera playerCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //camera.orthographicSize = 7.0f;
+    }
+
+}
